bbe4172b082198c61e7134a84505b519589b52a9
Jason Liang <jjjasonliang@gmail.com>
2019-03-01 23:03:37 +0530

    dynamically updating css

--- kanban/routes.py
+++ kanban/routes.py
@@ -1,7 +1,22 @@
 from flask import render_template, url_for, flash, redirect
 from kanban import app
 from kanban.models import Todo
+import os
 
 @app.route("/")
 def home():
 	return render_template('home.html')
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
