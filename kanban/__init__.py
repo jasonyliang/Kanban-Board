3a8522dddaf023f5188e250012a5fcfb4d08ff34
Jason Liang <jjjasonliang@gmail.com>
2019-03-13 15:07:17 +0530

    login manager

--- kanban/__init__.py
+++ kanban/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__, instance_relative_config=True)
 app.config['SECRET_KEY'] = 'a8c24e8ea6fb2993715e4c3a4aa8996d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+# login and user authentication 
+bcrypt = Bcrypt()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = "info"
 
 # has to put it down here to avoid circular importing
 from kanban import routes
