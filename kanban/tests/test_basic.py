8164bdf20bea56c5b233e873ee1f6b06eadfdbaf
Jason Liang <jjjasonliang@gmail.com>
2019-03-15 10:30:15 +0530

    user authentication unit tests

--- kanban/tests/test_basic.py
+++ kanban/tests/test_basic.py
@@ -28,20 +28,59 @@ class BasicTests(unittest.TestCase):
         self.app = app.test_client()
         db.drop_all()
         db.create_all()
+
  
 
  
     # executed after each test
     def tearDown(self):
         pass
+    
+    # user authentication methods
+     
+    def register(self, username, email, password, confirm):
+        return self.app.post(
+            '/register',
+            data=dict(username=username, email=email, password=password, confirm=confirm),
+            follow_redirects=True
+        )
+     
+    def login(self, email, password):
+        return self.app.post(
+            '/login',
+            data=dict(email=email, password=password),
+            follow_redirects=True
+        )
+     
+    def logout(self):
+        return self.app.get(
+            '/logout',
+            follow_redirects=True
+        )
  
- 
-###############
-#### tests ####
-###############
- 
+    #basic tests
+    # User authentication tests
+    def test_valid_user_registration(self):
+        response = self.register('test', 'test@test.com', 'test1', 'test1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_user_login(self):
+        response = self.login('test@test.com', 'test1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_user_logout(self):
+        response = self.logout()
+        self.assertEqual(response.status_code, 200)
+
+    # page tests
     def test_main_page(self):
+        #response = self.register('test@test.com', 'test1', 'test')
+        #self.login('test@test.com', 'test1')
         response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_todo_page(self):
+        response = self.app.get('/add', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
  
  
